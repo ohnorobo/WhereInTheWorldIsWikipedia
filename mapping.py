d79fa1cd721622620abc79d3f5bdbf0b9a5072e0
ohnorobo <sar.laplante@gmail.com>
2015-02-28 03:21:16 -0500

    mapping data

--- mapping.py
+++ mapping.py
@@ -11,22 +11,15 @@ import csv
 
 def read_in_shapefile():
 
-  '''
-  b = Basemap(projection="lcc", 
-              llcrnrlon=west,
-              llcrnrlat=south,
-              urcrnrlon=east,
-              urcrnrlat=north,
-              lat_0=(south+north)/2,
-              lon_0=(east+west)/2)
-  '''
   b = Basemap(projection="robin", lon_0=0, lat_0=0)
 
   print("reading in file")
   countries = b.readshapefile("./data/50/ne_50m_admin_0_countries", "countries") #secondary
   #r = shapefile.Reader("~/data/gadm_v2_shp/gadm2")
   print("read in file")
-  return countries
+  #b.drawcountries(color="#FFFFFF", linewidth=5)
+  #b.drawmapboundary(fill_color='#FFFFFF') #turn all boundaries white
+  return b
 
 
 
@@ -39,8 +32,8 @@ def read_in_data():
     lang, project, country, percent = row
 
     if not (lang, project) in data:
-      data[(lang, project)] = []
-    data[(lang, project)].append((country, percent))
+      data[(lang, project)] = {}
+    data[(lang, project)][country] = percent
 
   return data
 
@@ -48,39 +41,54 @@ def read_in_data():
 
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
-cm = plt.get_cmap('gist_rainbow') 
+cm = plt.get_cmap('brg') 
 cNorm  = colors.Normalize(vmin=0, vmax=100) #percentages
 scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
 
 
-def graph_a_thing(countries, values, language, project):
+def graph_a_thing(values, language, project):
+    m = read_in_shapefile()
+
     ax = plt.subplot(111)
     # shapes is the shapefile
     # values is a dict mapping of the GEOIDs in shapes to the heatmap values
-    shapes = countries[4]
+    shapes = m.countries
+    info = m.countries_info
+    used_names = []
 
-    for line in shapes:
+    #print(shapes)
+    #print(info[0])
+
+
+    for i in range(0, len(shapes)):
         # x and y are empty lists to be populated with the coords of each geometry.
         x = []
         y = []
 
-        #geoid = int(info['GEOID'])
-        #value = values[geoid]
 
-        for j in range(0,len(border)):
+        border = shapes[i]
+        name = info[i]['brk_name']
 
-            # get x and y coordinates.
-            tempx = float(border[j][0]) #x
-            tempy = float(border[j][1]) #y
-            x.append(tempx)
-            y.append(tempy) # Populate the lists  
+        if name in values: # and name not in used_names:
+          used_names.append(name)
+          value = int(values[name])
+          #print((name, value))
+          #print(info[i])
 
-        # Creates a polygon in matplotlib for each geometry in the shapefile
-        if value == 0:
-            color = (0.0, 0.0, 0.0, 0.0)
-        else:
-            color = scalarMap.to_rgba(value)
-        plt.fill(x,y, fc=color)
+          for j in range(0,len(border)):
+
+              # get x and y coordinates.
+              tempx = float(border[j][0]) #x
+              tempy = float(border[j][1]) #y
+              x.append(tempx)
+              y.append(tempy) # Populate the lists  
+
+          # Creates a polygon in matplotlib for each geometry in the shapefile
+          if value == 0:
+              color = (0.0, 0.0, 0.0, 0.0)
+          else:
+              color = scalarMap.to_rgba(value)
+          plt.fill(x,y, facecolor=color, edgecolor='none')
 
     plt.axis('equal')
     # This sets the x and y axes as equal intervals.
@@ -90,28 +98,28 @@ def graph_a_thing(countries, values, language, project):
     #plt.show() # Draws the map!
     plt.axis('off')
 
-    b1 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(600), width=0.4, align="edge")
-    b2 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(550), width=0.4, align="edge")
-    b3 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(500), width=0.4, align="edge")
-    b4 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(450), width=0.4, align="edge")
+    b1 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(90), width=0.4, align="edge")
+    b2 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(70), width=0.4, align="edge")
+    b3 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(50), width=0.4, align="edge")
+    b4 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(30), width=0.4, align="edge")
+    b5 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(10), width=0.4, align="edge")
 
-    ax.legend([b1, b2, b3, b4], ["600", "550", "500", "450"])
-    ax.set_title("Title")
+    ax.legend([b1, b2, b3, b4, b5], ["90%", "70%", "50%", "30%", "10%"], loc=3)
 
-    plt.savefig("state.png", figsize=(600,500), dpi=500)
+    title = language.decode('utf8') + " " + project.decode("utf8")
+    print(title)
+    print(set(used_names))
+    ax.set_title(title)
+
+    plt.savefig("./images/"+language+"-"+project+".png", figsize=(600,500), dpi=500)
 
 
 
 
 data = read_in_data()
-countries = read_in_shapefile()
-
-print(countries)
-
-plt.savefig("world.png", figsize=(400,200), dpi=1000)
-
 
 for language, project in data.keys():
-  graph_a_thing(countries, data[(language, project)], language, project)
+  #print((language, keys))
+  graph_a_thing(data[(language, project)], language, project)
 
 
