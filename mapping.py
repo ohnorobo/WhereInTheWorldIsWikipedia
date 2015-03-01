38c9ce41ea878ee0e3072460d44d57c6570c3b5a
ohnorobo <sar.laplante@gmail.com>
2015-03-01 01:32:23 -0500

    better scaling for colors

--- mapping.py
+++ mapping.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from pprint import pprint
 import csv
+from math import log
 #import shapefile
 
 
@@ -15,10 +16,7 @@ def read_in_shapefile():
 
   print("reading in file")
   countries = b.readshapefile("./data/50/ne_50m_admin_0_countries", "countries") #secondary
-  #r = shapefile.Reader("~/data/gadm_v2_shp/gadm2")
   print("read in file")
-  #b.drawcountries(color="#FFFFFF", linewidth=5)
-  #b.drawmapboundary(fill_color='#FFFFFF') #turn all boundaries white
   return b
 
 
@@ -41,36 +39,32 @@ def read_in_data():
 
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
-cm = plt.get_cmap('brg') 
-cNorm  = colors.Normalize(vmin=0, vmax=100) #percentages
+cm = plt.get_cmap('gist_rainbow') 
+cNorm  = colors.Normalize(vmin=0, vmax=log(100)) #percentages
 scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
 
 
-def graph_a_thing(values, language, project):
-    m = read_in_shapefile()
+def graph_a_thing(m, values, language, project):
+
+    m = read_in_shapefile() #this redraws the lines, but it's a silly way to do it
 
     ax = plt.subplot(111)
     # shapes is the shapefile
-    # values is a dict mapping of the GEOIDs in shapes to the heatmap values
     shapes = m.countries
     info = m.countries_info
     used_names = []
-
-    #print(shapes)
-    #print(info[0])
-
 
     for i in range(0, len(shapes)):
         # x and y are empty lists to be populated with the coords of each geometry.
         x = []
         y = []
 
-
         border = shapes[i]
         name = info[i]['brk_name']
 
-        if name in values: # and name not in used_names:
+        if name in values: 
           used_names.append(name)
+          #print((name, values[name]))
           value = int(values[name])
           #print((name, value))
           #print(info[i])
@@ -87,7 +81,7 @@ def graph_a_thing(values, language, project):
           if value == 0:
               color = (0.0, 0.0, 0.0, 0.0)
           else:
-              color = scalarMap.to_rgba(value)
+              color = scalarMap.to_rgba(log(value))
           plt.fill(x,y, facecolor=color, edgecolor='none')
 
     plt.axis('equal')
@@ -95,16 +89,19 @@ def graph_a_thing(values, language, project):
     # NB this script will only work for projected data, for geographical
     # coordinate systems get ready to do some maths  
 
-    #plt.show() # Draws the map!
     plt.axis('off')
 
-    b1 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(90), width=0.4, align="edge")
-    b2 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(70), width=0.4, align="edge")
-    b3 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(50), width=0.4, align="edge")
-    b4 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(30), width=0.4, align="edge")
-    b5 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(10), width=0.4, align="edge")
+    b1 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(log(100)), width=0.4, align="edge")
+    b2 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(log(50)), width=0.4, align="edge")
+    b3 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(log(25)), width=0.4, align="edge")
+    b4 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(log(12)), width=0.4, align="edge")
+    b5 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(log(6)), width=0.4, align="edge")
+    b6 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(log(3)), width=0.4, align="edge")
+    b7 = ax.bar([0, 1, 2], [0.2, 0.3, 0.1], color=scalarMap.to_rgba(log(1)), width=0.4, align="edge")
 
-    ax.legend([b1, b2, b3, b4, b5], ["90%", "70%", "50%", "30%", "10%"], loc=3)
+    ax.legend([b1, b2, b3, b4, b5, b6, b7], 
+              ["100%", "50%", "25%", "12%", "6%", "3%", "1%"],
+              loc=3)
 
     title = language.decode('utf8') + " " + project.decode("utf8")
     print(title)
@@ -112,14 +109,16 @@ def graph_a_thing(values, language, project):
     ax.set_title(title)
 
     plt.savefig("./images/"+language+"-"+project+".png", figsize=(600,500), dpi=500)
+    plt.clf()
 
 
 
 
+m = read_in_shapefile()
 data = read_in_data()
 
-for language, project in data.keys():
-  #print((language, keys))
-  graph_a_thing(data[(language, project)], language, project)
+for language, project in sorted(data.keys()):
+  print((language, project))
+  graph_a_thing(m, data[(language, project)], language, project)
 
 
